bb9c3fd803f776b01d7e096786c9a93f024469fb
karl <karl@macc.io>
2016-09-06 13:01:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/karlmorris/slider/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.karlmorris.slider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ArrayList a = new ArrayList<>();
+
+
+    }
+}
