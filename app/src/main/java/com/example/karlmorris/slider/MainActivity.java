aaa4c838e96a39f0f0fef91b52252d389bd35161
karl <karl@macc.io>
2016-09-06 13:45:23 -0400

    Implemented slider event listener.

--- app/src/main/java/com/example/karlmorris/slider/MainActivity.java
+++ app/src/main/java/com/example/karlmorris/slider/MainActivity.java
@@ -2,19 +2,45 @@ package com.example.karlmorris.slider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        message = (TextView) findViewById(R.id.textView);
+        SeekBar sb = (SeekBar) findViewById(R.id.seekBar);
 
-        ArrayList a = new ArrayList<>();
+        sb.setProgress(100);
 
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                float alphaVal = (float) progress / 100;
+
+                message.setAlpha(alphaVal);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 }
